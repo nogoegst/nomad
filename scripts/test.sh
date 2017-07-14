98f67469a3737930f97c5815074de107edd815bc
Ivan Markin <twim@riseup.net>
2017-07-14 12:21:50 +0000

    Use doas(1) on OpenBSD instead of sudo; use env(1) to pass environment variables

--- scripts/test.sh
+++ scripts/test.sh
@@ -20,6 +20,11 @@ go build -i -tags "$GOTEST_TAGS" -o $TEMPDIR/nomad || exit 1
 # Run the tests
 echo "--> Running tests"
 GOBIN="`which go`"
-sudo -E PATH=$TEMPDIR:$PATH  -E GOPATH=$GOPATH   -E NOMAD_TEST_RKT=1 \
+
+SUDO="sudo"
+if [[ $(uname) == "OpenBSD" ]]; then
+	SUDO="doas"
+fi
+$SUDO env PATH=$TEMPDIR:$PATH  GOPATH=$GOPATH  NOMAD_TEST_RKT=1 \
     $GOBIN test -tags "$GOTEST_TAGS" ${GOTEST_FLAGS:--cover -timeout=900s} $($GOBIN list ./... | grep -v /vendor/)
 
